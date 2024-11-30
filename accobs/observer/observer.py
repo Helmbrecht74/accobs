b4346b37d214c45b07bcc41d75f9382a32976533
Peter L <46744164+Helmbrecht74@users.noreply.github.com>
2024-11-30 15:01:44 +0100

    Observer initialized

---
+++ accobs/observer/observer.py
@@ -0,0 +1,15 @@
+
+
+class observer:
+    def __init__(self, state_set, alphabet, transitions, output, initial_state):
+        self.state_set = frozenset(state_set)
+        self.alphabet = frozenset(alphabet)
+        self.intial_state = initial_state
+        self.output = output
+        self.transitions = transitions
+        
+    def run_on_string(self, string):
+        current_state = self.intial_state
+        for symbol in string:
+            current_state = self.transitions[current_state][symbol]
+        return self.output[current_state]
--- accobsTests.py
+++ accobsTests.py
@@ -27,3 +27,19 @@ print(rw.move_one())
 #rw = srs.srs( rules )
 rw.set_start_string("a")
 print(rw.move_one())
+
+
+from accobs.observer.observer import observer as obs 
+
+states = { 'q1', 'q2' }
+alph = { 'a', 'b' }
+transitions = {
+    'q1' : { 'a' : 'q1', 'b' : 'q2' },
+    'q2' : { 'a' : 'q2', 'b' : 'q2' },
+    }
+outputf = { 'q1' : 'A', 'q2' : 'B' }
+
+ob = obs( states, alph, transitions, outputf, 'q1')
+
+print(ob.run_on_string('aaaaa'))
+print(ob.run_on_string('aababbaa'))
